6f2771410496c6ab2c1011c90e6c909a85e64805
ezrleen <boughanmiaziz404@gmail.com>
2024-12-03 20:44:05 +0000

    setting up the login page with js

---
+++ index.js
@@ -0,0 +1,8 @@
+import { generateLogin } from "./pages/Login.js";
+
+const loginButton = document.getElementById("login");
+const container = document.querySelector(".container");
+
+loginButton.addEventListener("click", () => {
+    container.innerHTML = generateLogin();
+});
