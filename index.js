420157d9d3172248b45f03f2423118a1591a2174
ezrleen <boughanmiaziz404@gmail.com>
2024-12-04 01:39:12 +0000

    added carousel using bootstrap in index.html+styling it

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const loginButton = document.getElementById("login");
 const container = document.querySelector(".container");
 
 loginButton.addEventListener("click", () => {
+    container.innerHTML ="";
     container.innerHTML = generateLogin();
     document.body.style.background = "url('login.jpg')";
     document.body.style.backgroundRepeat = "no-repeat";
