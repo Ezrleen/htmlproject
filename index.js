40f5afce1f0f245cdbee95fd415ae10e3250372b
ezrleen <boughanmiaziz404@gmail.com>
2024-12-04 02:29:51 +0000

    done with the first part of the index webpack

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 import { generateLogin } from "./pages/Login.js";
+import { generatemain } from "./pages/main.js";
 
 const loginButton = document.getElementById("login");
+const mainButton = document.getElementById("main");
 const container = document.querySelector(".container");
 
+
 loginButton.addEventListener("click", () => {
-    container.innerHTML ="";
-    container.innerHTML = generateLogin();
-    document.body.style.background = "url('login.jpg')";
+    container.innerHTML = "";
+    container.innerHTML = generateLogin(); 
+    document.body.style.background = "url('./images/login.jpg')"; // 
     document.body.style.backgroundRepeat = "no-repeat";
     document.body.style.backgroundSize = "cover";
 });
+
+
+mainButton.addEventListener("click", () => {
+    container.innerHTML = ""; 
+    container.innerHTML = generatemain();
+    document.body.style.background = "url('./images/EaUnExCU8AAalKe.jpg')"; 
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundSize = "cover";
+});
+window.onload=(() => {
+    
+    container.innerHTML = generatemain();
+});
+window.addEventListener("")
